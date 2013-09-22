06b27e014f97c70410d6e2190d566b0d8dc51678
Grant Muller <grantmuller@gmail.com>
2013-09-22 16:30:38 -0400

    adding a proper test page for the load balancer

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var app = express();
 
 app.use(express.static('app'))
 app.get('/fetch', image_proxy());
+app.get('/test', function(req, res) {
+    res.writeHead(200);
+    res.write('Ok');
+    res.end();
+});
 
 app.listen(3000);
 console.log('Listening on port 3000');
